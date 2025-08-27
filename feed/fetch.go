6f466b19a565881319204ed29c40fd0621412064
William Henrik Behn <behnwilliam@icloud.com>
2025-08-27 20:42:05 +0200

    Refaktorer fetch.go, fiks hvilke html elementer som bir hentet fra beskjed-siden

--- feed/fetch.go
+++ feed/fetch.go
@@ -10,13 +10,52 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-func Fetch(course string) []string {
+func fetchRssFeed(course string) *gofeed.Feed {
 	fp := gofeed.NewParser()
 
 	feed, err := fp.ParseURL("https://www.uio.no/studier/emner/matnat/ifi/" + course + "/h25/beskjeder/?vrtx=feed")
 	if err != nil {
 		return nil
 	}
+
+	return feed
+}
+
+func fetchHttpItem(resp *http.Response) string {
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println("Html parse error", err)
+		return ""
+	}
+
+	htmlHeader := doc.Find("h1")
+	htmlContent := doc.Find("#vrtx-field-message")
+
+	if htmlHeader.Length() == 0 || htmlContent.Length() == 0 {
+		fmt.Println("Error finding html tag(s)")
+		return ""
+	}
+
+	htmlH, _ := htmlHeader.Html()
+	htmlC, _ := htmlContent.Html()
+
+	return "<h1>" + htmlH + "</h1>" + htmlC
+}
+
+func convertToMarkdown(html string) string {
+	converter := h2m.NewConverter("", true, nil)
+	markdown, err := converter.ConvertString(html)
+
+	if err != nil {
+		fmt.Println("Error converting html to markdown:", err)
+		return ""
+	}
+	return markdown
+}
+
+func Fetch(course string) []string {
+
+	feed := fetchRssFeed(course)
 
 	var results []string
 
@@ -29,29 +68,8 @@ func Fetch(course string) []string {
 		}
 		defer resp.Body.Close()
 
-		doc, err := goquery.NewDocumentFromReader(resp.Body)
-		if err != nil {
-			fmt.Println("Html parse error", err)
-			continue
-		}
-
-		htmlSelection := doc.Find("#right-main")
-		if htmlSelection.Length() == 0 {
-			fmt.Println("Error findin html tag", item.Link)
-			continue
-		}
-
-		html, err := htmlSelection.Html()
-		if err != nil {
-			continue
-		}
-
-		converter := h2m.NewConverter("", true, nil)
-		markdown, err := converter.ConvertString(html)
-		if err != nil {
-			fmt.Println("Error converting html to markdown:", err)
-			continue
-		}
+		html := fetchHttpItem(resp)
+		markdown := convertToMarkdown(html)
 
 		out, err := glamour.Render(markdown, "dark")
 		if err != nil {
@@ -60,6 +78,8 @@ func Fetch(course string) []string {
 		}
 
 		results = append(results, out)
+
+		resp.Body.Close()
 	}
 	return results
 }
